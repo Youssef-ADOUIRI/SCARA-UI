4e768b4db43cdbbaf6eefae199aea2ffbcc79671
Youssef Adouiri Alaoui <youssef.galaoui@gmail.com>
2021-11-28 15:36:07 +0100

    Add project files.

---
+++ SCARA UI/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using SCARA_UI.core;
+using System;
+
+namespace SCARA_UI.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+
+        public HomeViewModel HomeVM { get; set; }
+
+        private object _currentView;
+
+        public object CurentView
+        {
+            get { return _currentView; }
+            set { 
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            CurentView = HomeVM;
+        }
+    }
+}
---
+++ SCARA UI/core/RelayCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SCARA_UI.core
+{
+    class RelayCommand : ICommand
+    {
+        private Action<object> _execute;
+        private Func<Object, bool> _canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action<object> execute, Func<Object, bool> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parametre)
+        {
+            return _canExecute == null || _canExecute(parametre);
+        }
+
+        public void Execute(object parametre)
+        {
+            _execute(parametre);
+        }
+    }
+}
